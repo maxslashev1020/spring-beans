b4db2310c6959b2a51e223e8aac321365022175f
maxslashev <maxslahev1020@gmail.com>
2021-08-16 16:07:32 +0400

    12w3

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,35 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AnimalsCage {
+public class AnimalsCage extends Timer{
+
+    private Animal animal;
+    private Timer timer;
+
+    public Timer getTimer() {
+        return this;
+    }
+    @Override
+    public Long getTime() {
+        return super.getTime();
+    }
 
     @Autowired
-    private Animal animal;
+    public AnimalsCage(@Qualifier("dog") Animal animal, Timer timer) {
+        this.animal = animal;
+        this.timer = timer;
+    }
+
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 }
